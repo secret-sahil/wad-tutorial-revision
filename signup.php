15fb402b363d12dd5710f59a2b3c2a5a13392673
Sahil Kumar <sahilkumar142002@gmail.com>
2022-01-27 23:24:23 +0530

    connected login with sql

--- signup.php
+++ signup.php
@@ -1,14 +1,58 @@
 <?php 
 include "include/header.php";
 ?>
+<?php
+$msg= new ArrayObject(array());
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name=$_POST['name'];
+    $mail=$_POST['email'];
+    $gender=$_POST['gender'];
+    $pwd=$_POST['pwd'];
+    $cpwd=$_POST['cpwd'];
+    $dob=$_POST['dob'];
+    
+
+    if (empty($name) or empty($mail) or empty($gender) or empty($pwd) or empty($cpwd) or empty($dob)) {
+        $msg->append('All fields are required');
+    }
+    elseif ($pwd != $cpwd) {
+        $msg->append('Password do not match');
+    } 
+    else{
+        require_once "include/db.php";
+        $query=$db->prepare('INSERT into users(name , gender, dob, email, password) VALUES(?, ?, ?, ?, ?)');
+        $query->execute(array(
+            $name,
+            $gender,
+            $dob,
+            $mail,
+            $pwd
+        ));
+        $msg->append('Registered Sucessfully! <a href="login.php">Login</a>');
+    }
+} ?>
 	<body>
     <?php  include "include/nav.php"; ?>
     <div class="main">
         <div class="myform">
-            <form action="signup_process.php" method="post">
+            <form action="signup.php" method="post">
+                <?php
+                            for ($i=0; $i < sizeof($msg); $i++) {
+                                if ($msg[$i]=='Registered Sucessfully! <a href="login.php">Login</a>') { ?>
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                        <?php echo $msg[$i]; ?>
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                <?php } else { ?>
+                                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <?php echo $msg[$i]; ?>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                                <?php }
+                            }?>
                 <div class="input-group mb-3">
                     <label class="input-group-text" for="inputGroupSelect01">Full Name</label>
-                    <input type="text" name='fullname' class="form-control">
+                    <input type="text" name='name' class="form-control">
                 </div>
                 <div class="input-group mb-3">
                     <label class="input-group-text" for="inputGroupSelect01">Gender</label>
@@ -41,4 +85,3 @@ include "include/header.php";
     <?php  include "include/footer.php"; ?>
 	</body>
 </html>
-
--- signup_process.php
+++
@@ -1,4 +0,0 @@
-<?php 
-// code here
-
-?>
